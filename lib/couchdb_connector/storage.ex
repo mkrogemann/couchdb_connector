8f4d1692fc6f35f0fcd8601e24112cde950efbc6
Markus Krogemann <markus@krogemann.de>
2015-12-27 21:31:00 +0100

    document the Storage module

--- lib/couchdb_connector/storage.ex
+++ lib/couchdb_connector/storage.ex
@@ -1,10 +1,25 @@
 defmodule Couchdb.Connector.Storage do
-  @moduledoc false
+  @moduledoc """
+  The Storage module provides functions to create and destroy databases.
+
+  ## Examples
+
+      iex>db_props = %{protocol: "http", hostname: "localhost",database: "couchdb_connector_test", port: 5984}
+      %{database: "couchdb_connector_test", hostname: "localhost", port: 5984, protocol: "http"}
+      iex>Couchdb.Connector.storage_up(db_props)
+      {:ok, "{\\"ok\\":true}\\n"}
+      iex>Couchdb.Connector.storage_down(db_props)
+      {:ok, "{\\"ok\\":true}\\n"}
+
+  """
 
   alias Couchdb.Connector.Headers
   alias Couchdb.Connector.UrlHelper
   alias Couchdb.Connector.ResponseHandler, as: Handler
 
+  @doc """
+  Create a database with parameters as given in the db_props map.
+  """
   def storage_up db_props do
     db_props
     |> UrlHelper.database_url
@@ -12,6 +27,9 @@ defmodule Couchdb.Connector.Storage do
     |> Handler.handle_put
   end
 
+  @doc """
+  Delete the database with the properties as given in the db_props map.
+  """
   def storage_down db_props do
     db_props
     |> UrlHelper.database_url

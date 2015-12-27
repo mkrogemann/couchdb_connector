6c0f7d9fa07cf07f9c72b6110c60576843eba481
Markus Krogemann <markus@krogemann.de>
2015-12-27 20:56:52 +0100

    document Writer module

--- lib/couchdb_connector/writer.ex
+++ lib/couchdb_connector/writer.ex
@@ -1,17 +1,48 @@
 defmodule Couchdb.Connector.Writer do
-  @moduledoc false
+  @moduledoc """
+  The Writer module provides functions to create and update documents in
+  the CouchDB database given by the database properties.
+
+  ## Examples
+
+      db_props = %{protocol: "http", hostname: "localhost",database: "couchdb_connector_test", port: 5984}
+      %{database: "couchdb_connector_test", hostname: "localhost", port: 5984, protocol: "http"}
+
+      Couchdb.Connector.storage_up(db_props)
+      {:ok, "{\\"ok\\":true}\\n"}
+
+      Couchdb.Connector.create(db_props, "{\\"key\\": \\"value\\"}")
+      {:ok, "{\\"ok\\":true,\\"id\\":\\"7dd00...\\",\\"rev\\":\\"1-59414e7...\\"}\\n",
+            [{"Server", "CouchDB/1.6.1 (Erlang OTP/18)"},
+             {"Location", "http://localhost:5984/couchdb_connector_test/7dd00..."},
+             {"ETag", "\\"1-59414e7...\\""}, {"Date", "Mon, 21 Dec 2015 16:13:23 GMT"},
+             {"Content-Type", "text/plain; charset=utf-8"},
+             {"Content-Length", "95"}, {"Cache-Control", "must-revalidate"}]}
+  """
 
   alias Couchdb.Connector.Headers
   alias Couchdb.Connector.Reader
   alias Couchdb.Connector.UrlHelper
   alias Couchdb.Connector.ResponseHandler, as: Handler
 
+  @doc """
+  Create a new document with given json and a CouchDB generated id.
+  This function maps to a HTTP PUT.
+  Fetching the uuid from CouchDB does incur a performance penalty as
+  compared to providing one and using create/3.
+  """
   def create db_props, json do
     { :ok, uuid_json } = Reader.fetch_uuid(db_props)
     uuid = hd(Poison.decode!(uuid_json)["uuids"])
     create db_props, json, uuid
   end
 
+  @doc """
+  Create a new document with given json and given id. Clients must make sure
+  that the id has not been used for an existing document in CouchDB.
+  Either provide a UUID or consider using create/2 in case uniqueness cannot
+  be guaranteed.
+  """
   def create db_props, json, id do
     db_props
     |> UrlHelper.document_url(id)
@@ -33,6 +64,10 @@ defmodule Couchdb.Connector.Writer do
     end
   end
 
+  @doc """
+  Update the given document. Note that an _id field must be contained in the
+  document. A missing _id field with trigger a RuntimeError.
+  """
   def update db_props, json do
     doc_map = Poison.Parser.parse!(json)
     id = Map.fetch(doc_map, "_id")
@@ -51,6 +86,11 @@ defmodule Couchdb.Connector.Writer do
     HTTPoison.put! url, Poison.encode!(doc_map), [ Headers.json_header ]
   end
 
+  @doc """
+  Update the given document that is stored under the given id. Note that
+  a mismatch between the id parameter and the _id field contained in the
+  document will trigger a RuntimeError.
+  """
   def update db_props, json, id do
     db_props
     |> UrlHelper.document_url(id)

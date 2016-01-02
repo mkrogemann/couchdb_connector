7f6aa84812f0f4e03a702c302de98373d3830128
Markus Krogemann <markus@krogemann.de>
2016-01-02 14:09:14 +0100

    switch back to ~> 1.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Couchdb.Connector.Mixfile do
     [
       app: :couchdb_connector,
       version: "0.1.0",
-      elixir: "~> 1.2",
+      elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description,

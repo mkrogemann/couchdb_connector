de67eb063dab6f73b007573eaa0c7512d906e78f
Markus Krogemann <markus@krogemann.de>
2016-01-02 11:47:06 +0100

    fix tests for Elixir 1.2
    should still work with Elixir 1.1

--- test/couchdb_connector/writer_test.exs
+++ test/couchdb_connector/writer_test.exs
@@ -14,14 +14,14 @@ defmodule Couchdb.Connector.WriterTest do
     {:ok, body, headers} = Writer.create TestConfig.database_properties, "{\"key\": \"value\"}", "42"
     {:ok, body_map} = Poison.decode body
     assert body_map["id"] == "42"
-    assert id_from_url(headers["Location"]) == "42"
+    assert id_from_url(header_value(headers, "Location")) == "42"
   end
 
   test "create/3: ensure that given id overrides id contained in document" do
     {:ok, response_body, headers} = Writer.create TestConfig.database_properties, "{\"_id\": \"some_id\", \"key\": \"value\"}", "42"
     response_map = Poison.decode! response_body
     assert response_map["id"] == "42"
-    assert id_from_url(headers["Location"]) == "42"
+    assert id_from_url(header_value(headers, "Location")) == "42"
   end
 
   test "create/2: ensure that a new document gets create with a fetched id" do
@@ -33,11 +33,11 @@ defmodule Couchdb.Connector.WriterTest do
 
   test "update/2: ensure that a document that contains an existing id can be updated" do
     {:ok, _body, headers} = Writer.create TestConfig.database_properties, "{\"key\": \"original value\"}"
-    id = id_from_url(headers["Location"])
-    revision = headers["ETag"]
+    id = id_from_url(header_value(headers, "Location"))
+    revision = header_value(headers, "ETag")
     update = "{\"_id\": \"#{id}\", \"_rev\": #{revision}, \"key\": \"new value\"}"
     {:ok, _body, headers} = Writer.update TestConfig.database_properties, update
-    assert String.starts_with?(headers["ETag"], "\"2-")
+    assert String.starts_with?(header_value(headers, "ETag"), "\"2-")
   end
 
   test "update/2: verify that a document without id raises an exception" do
@@ -49,21 +49,25 @@ defmodule Couchdb.Connector.WriterTest do
 
   test "update/3: ensure that an existing document with given id can be updated" do
     {:ok, _body, headers} = Writer.create TestConfig.database_properties, "{\"key\": \"original value\"}"
-    id = id_from_url(headers["Location"])
-    revision = headers["ETag"]
+    id = id_from_url(header_value(headers, "Location"))
+    revision = header_value(headers, "ETag")
     update = "{\"_id\": \"#{id}\", \"_rev\": #{revision}, \"key\": \"new value\"}"
     {:ok, _body, headers} = Writer.update TestConfig.database_properties, update, id
-    assert String.starts_with?(headers["ETag"], "\"2-")
+    assert String.starts_with?(header_value(headers, "ETag"), "\"2-")
   end
 
   test "update/3: verify that a mismatch of document id and URL id raises an exception" do
     {:ok, _body, headers} = Writer.create TestConfig.database_properties, "{\"key\": \"original value\"}"
-    id = id_from_url(headers["Location"])
-    revision = headers["ETag"]
+    id = id_from_url(header_value(headers, "Location"))
+    revision = header_value(headers, "ETag")
     update = "{\"_id\": \"some_wrong_id\", \"_rev\": #{revision}, \"key\": \"new value\"}"
     assert_raise RuntimeError, fn ->
       Writer.update TestConfig.database_properties, update, id
     end
+  end
+
+  defp header_value headers, key do
+    Enum.into(headers, %{})[key]
   end
 
   defp id_from_url url do

57a6135e2e7901a9e50416b6cf749e86873e02d3
Markus Krogemann <markus@krogemann.de>
2016-04-01 01:15:30 +0200

    adjust formatting for readability

--- lib/couchdb_connector/response_handler.ex
+++ lib/couchdb_connector/response_handler.ex
@@ -1,19 +1,17 @@
 defmodule Couchdb.Connector.ResponseHandler do
   @moduledoc false
 
-  def handle_get(%{status_code: 200, body: body}), do: { :ok, body }
-  def handle_get(%{status_code: ___, body: body}), do: { :error, body }
+  def handle_get(%{status_code: 200, body: body}),    do: {:ok,    body}
+  def handle_get(%{status_code: ___, body: body}),    do: {:error, body}
 
-  def handle_put(%{status_code: 201, body: body}), do: { :ok, body }
-  def handle_put(%{status_code: ___, body: body}), do: { :error, body }
+  def handle_delete(%{status_code: 200, body: body}), do: {:ok,    body}
+  def handle_delete(%{status_code: ___, body: body}), do: {:error, body}
 
-  def handle_put(
-    %{status_code: 201, body: body, headers: headers},
-    _include_headers = true), do: { :ok, body, headers }
-  def handle_put(
-    %{status_code: ___, body: body, headers: headers},
-    _include_headers = true), do: { :error, body, headers }
+  def handle_put(%{status_code: 201, body: body}),    do: {:ok,    body}
+  def handle_put(%{status_code: ___, body: body}),    do: {:error, body}
 
-  def handle_delete(%{status_code: 200, body: body}), do: { :ok, body }
-  def handle_delete(%{status_code: ___, body: body}), do: { :error, body }
+  def handle_put(%{status_code: 201, body: body, headers: headers},
+                 _include_headers = true),            do: {:ok,    body, headers}
+  def handle_put(%{status_code: ___, body: body, headers: headers},
+                 _include_headers = true),            do: {:error, body, headers}
 end

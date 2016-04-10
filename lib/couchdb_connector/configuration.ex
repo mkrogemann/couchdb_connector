0680aea2243fbd41fc6e444e79ef958f02b647be
Markus Krogemann <markus@krogemann.de>
2016-04-11 00:15:32 +0200

    add documentation

--- lib/couchdb_connector/configuration.ex
+++ lib/couchdb_connector/configuration.ex
@@ -1,11 +1,15 @@
 defmodule Couchdb.Connector.Configuration do
   @moduledoc """
-  TODO
+  Provides functions to access the server's configuration.
   """
 
   alias Couchdb.Connector.UrlHelper
   alias Couchdb.Connector.ResponseHandler, as: Handler
 
+  @doc """
+  Returns complete server configuration.
+  """
+  @spec get(db_properties, String.t, String.t) :: String.t
   def get db_props, admin_name, password do
     db_props
     |> UrlHelper.config_url(admin_name, password)

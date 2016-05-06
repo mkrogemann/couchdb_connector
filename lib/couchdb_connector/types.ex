0a0bf9ea8bc1c16e03e0e94470d2b657c7e0cc29
Markus Krogemann <markus@krogemann.de>
2016-05-06 21:53:37 +0200

    Add a type for basic authentication credentials

--- lib/couchdb_connector/types.ex
+++ lib/couchdb_connector/types.ex
@@ -1,5 +1,8 @@
 defmodule Couchdb.Connector.Types do
-  @moduledoc false
+  @moduledoc """
+  The types module contains only type definitions that other modules can
+  make use of by simply using this modules: 'use Couchdb.Connector.Types'
+  """
 
   @doc false
   defmacro __using__(_opts) do
@@ -7,7 +10,8 @@ defmodule Couchdb.Connector.Types do
       @type db_properties :: %{protocol: String.t, hostname: String.t,
                                database: String.t, port: non_neg_integer}
       @type user_roles_list :: [String.t]
-      @type headers :: [{String.t, String.t}]
+      @type headers :: [{String.t, String.t}] # List of header names and values
+      @type basic_auth :: {String.t, String.t} # username, password
     end
   end
 end

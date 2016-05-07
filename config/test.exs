a666de7244a57c3bf7002beb71c4ba9e5c3316e7
Markus Krogemann <markus@krogemann.de>
2016-05-07 11:08:52 +0200

    Remove authentication settings from config
    Authentication parameters have to be provided by clients. We no longer
    attempt to provide an Agent-based configuration within the connector
    application.

--- config/test.exs
+++ config/test.exs
@@ -4,9 +4,4 @@ config :couchdb_connector,
   protocol: "http",
   hostname: "127.0.0.1",
   database: "couchdb_connector_test",
-  port: 5984,
-  authentication: :basic_auth,
-  username: "jan",
-  password: "relax",
-  adminname: "anna",
-  adminpwd: "secret"
+  port: 5984

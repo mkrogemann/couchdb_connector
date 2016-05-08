22aa7c8c2f507080846884f908847b6aab4e1302
Markus Krogemann <markus@krogemann.de>
2016-05-08 21:43:11 +0200

    Split reader test into secured and unsecured cases

--- test/couchdb_connector/secure_reader_test.exs
+++ test/couchdb_connector/secure_reader_test.exs
@@ -1,4 +1,4 @@
-defmodule Couchdb.Connector.ReaderTest do
+defmodule Couchdb.Connector.SecureReaderTest do
   use ExUnit.Case
 
   alias Couchdb.Connector.Reader
@@ -9,25 +9,23 @@ defmodule Couchdb.Connector.ReaderTest do
     TestPrep.ensure_database
     TestPrep.ensure_document "{\"test_key\": \"test_value\"}", "foo"
     on_exit context, fn ->
+      TestPrep.delete_test_user
+      TestPrep.delete_test_admin
       TestPrep.delete_database
     end
   end
 
-  # Test cases for unsecured database
+  # Tests for secured database, using basic authentication
 
-  test "get/2: ensure that document exists" do
-    { :ok, json } = Reader.get TestConfig.database_properties, "foo"
+  test "get/3: ensure that document exists using basic authentication" do
+    TestPrep.secure_database
+    { :ok, json } = Reader.get(TestConfig.database_properties, {"jan", "relax"}, "foo")
     { :ok, json_map } = Poison.decode json
     assert json_map["test_key"] == "test_value"
   end
 
-  test "get/2: ensure an error is returned for missing document" do
-    { :error, json } = Reader.get TestConfig.database_properties, "unicorn"
-    { :ok, json_map } = Poison.decode json
-    assert json_map["reason"] == "missing"
-  end
-
-  test "fetch_uuid/1: get a single uuid from database server" do
+  test "fetch_uuid/1: get a single uuid from a secured database server" do
+    TestPrep.secure_database
     { :ok, json } = Reader.fetch_uuid(TestConfig.database_properties)
     uuid = hd(Poison.decode!(json)["uuids"])
     assert String.length(uuid) == 32

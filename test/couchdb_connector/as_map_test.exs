bdf3698f99ceae013076bb5ef99f5615c38c61a9
Markus Krogemann <markus@krogemann.de>
2016-09-21 11:21:25 +0200

    Fix function arity in test names

--- test/couchdb_connector/as_map_test.exs
+++ test/couchdb_connector/as_map_test.exs
@@ -3,7 +3,7 @@ defmodule Couchdb.Connector.AsMapTest do
 
   import Couchdb.Connector.AsMap
 
-  test "as_map/2 with invalid json string should raise RuntimeError" do
+  test "as_map/1 with invalid json string should raise RuntimeError" do
     invalid = "{\"_id\":\"foo\",\"_rev\":\"1-0f97561a543ed2e9c98a24dea818ec10\",test_key\":\"test_value\"}\n"
 
     assert_raise(RuntimeError, """
@@ -13,7 +13,7 @@ defmodule Couchdb.Connector.AsMapTest do
     """, fn -> as_map(invalid) end)
   end
 
-  test "as_map/2 with empty string should raise RuntimeError" do
+  test "as_map/1 with empty string should raise RuntimeError" do
     empty = ""
 
     assert_raise(RuntimeError, """
@@ -23,7 +23,7 @@ defmodule Couchdb.Connector.AsMapTest do
     fn -> as_map(empty) end)
   end
 
-  test "as_map/2 with valid json string should return decoded Map" do
+  test "as_map/1 with valid json string should return decoded Map" do
     valid = "{\"_id\":\"foo\",\"_rev\":\"1-0f97561a543ed2e9c98a24dea818ec10\",\"test_key\":\"test_value\"}\n"
     decoded = as_map(valid)
 

f9c5115022abb5186465f03d22ab203a4171b178
Markus Krogemann <markus@krogemann.de>
2016-11-19 18:40:20 +0100

    Allow using poison 3.0 in addition to 1.5 and 2.0

--- test/couchdb_connector/as_map_test.exs
+++ test/couchdb_connector/as_map_test.exs
@@ -6,21 +6,13 @@ defmodule Couchdb.Connector.AsMapTest do
   test "as_map/1 with invalid json string should raise RuntimeError" do
     invalid = "{\"_id\":\"foo\",\"_rev\":\"1-0f97561a543ed2e9c98a24dea818ec10\",test_key\":\"test_value\"}\n"
 
-    assert_raise(RuntimeError, """
-    Document returned by CouchDB is invalid
-    token: t
-    json: {"_id":"foo","_rev":"1-0f97561a543ed2e9c98a24dea818ec10",test_key":"test_value"}\n
-    """, fn -> as_map(invalid) end)
+    assert_raise(RuntimeError, fn -> as_map(invalid) end)
   end
 
   test "as_map/1 with empty string should raise RuntimeError" do
     empty = ""
 
-    assert_raise(RuntimeError, """
-    Document returned by CouchDB is invalid
-    json: #{empty}
-    """,
-    fn -> as_map(empty) end)
+    assert_raise(RuntimeError, fn -> as_map(empty) end)
   end
 
   test "as_map/1 with valid json string should return decoded Map" do

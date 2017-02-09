42ee102e71c72d4c29fc2814c93e7fc98b7c8efb
Markus Krogemann <markus@qixxit.de>
2017-02-09 23:52:04 +0100

    Add function specs to supervisor

--- lib/couchdb_connector/supervisor.ex
+++ lib/couchdb_connector/supervisor.ex
@@ -4,14 +4,17 @@ defmodule Couchdb.Connector.Supervisor do
   The main supervisor of the couchdb_connector application
   """
 
+  @spec start_link(String.t) :: {:ok, pid()}
   def start_link(name) do
     Supervisor.start_link(__MODULE__, :ok, name: name)
   end
 
+  @spec start(term, map()) :: {:ok, pid()}
   def start(_type, args) do
     start_link(args[:name])
   end
 
+  @spec init(:ok) :: {:ok, {:supervisor.sup_flags, [Supervisor.Spec.spec]}}
   def init(:ok) do
     children = []
     supervise(children, strategy: :one_for_one)

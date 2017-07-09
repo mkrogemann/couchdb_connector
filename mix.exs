93c56ce37c432ea44b1165343eaf3dba33a46bed
Markus Krogemann <markus@qixxit.de>
2017-07-09 19:12:38 +0200

    Re-add Elixir 1.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Couchdb.Connector.Mixfile do
     [
       app: :couchdb_connector,
       version: "0.5.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
